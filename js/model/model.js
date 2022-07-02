517bf013c0441a497b131d6c6dbfdb5f28e6cfc3
Arzu <oreganojuice@gmail.com>
2022-07-02 21:02:34 +0300

    some changes and updates, app ready for styling

--- js/model/model.js
+++ js/model/model.js
@@ -1,13 +1,7 @@
 class Model{
     constructor() {
-        this.journal = new Journal();
         this.activities = new Activities();
     }
-
-    getJournal() {
-        return this.journal
-    }
-
     getActivities(){
         return this.activities
     }
